146b2ad6b68c1ecfe300bb2259b1c33cc069e2bb
albelizGH <alejobeliz@hotmail.com>
2023-10-29 17:06:47 -0300

    Se agregaron las funciones al archivo funciones.js

---
+++ funciones.js
@@ -0,0 +1,23 @@
+function suma(a,b){
+return a+b
+}
+
+function resta(a,b){
+    return a-b
+}
+
+function palabras(){
+    let texto=prompt("Ingrese su texto")
+    let arrayTexto=[];
+    arrayTexto=texto.split(/[ ,.;¿?!¡]/)
+    let cantidad=promt("Ingrese la cantidad de palabras que quiere obtener")
+
+    if(cantidad>arrayTexto.length){
+        console.log("Cantidad erronea")
+        return
+    }
+
+    for(i=0;i<cantidad;i++){
+        console.log(`${i+1}) ${arrayTexto[i]} `)
+    }
+}
